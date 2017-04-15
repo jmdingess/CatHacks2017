1610d4c905937168801399df4d2109aa5b153eb6
Jonathan Dingess <J_And_M33@yahoo.com>
2017-04-15 17:05:14 -0400

    More css

--- pages/main_page.php
+++ pages/main_page.php
@@ -6,9 +6,20 @@
 		
 		<?php
 			//Headers here
+			if (!empty($_POST)) {
+				if (isset($_POST['meme'])) {
+					$meme = $_POST['meme'];
+					
+				}
+			}
+			else {
+				$meme = 0;
+			}
 		?>
-		<link rel="stylesheet" href="../css/main_page.css" type="text/css">
 		
+		<!-- css stylesheets -->
+		<link rel="stylesheet" href="../css/main_page.css" type="text/css">
+		<link rel="stylesheet" href="../css/main.css" type="text/css">
 		
 	</head>
 	<body>
@@ -17,6 +28,20 @@
 				<h1>
 					Suck My Donkey Dong
 				</h1>
+			</div>
+			<div class="main-page-content">
+				<?php
+					if ($meme==1) {
+						echo
+							'<div class="fuck-you">
+								Fuck you
+								
+							</div>';
+					}
+				?>
+				<form action="" method="post">
+					<input type="submit" value=1 name="meme">
+				</form>
 			</div>
 		</div>
 	</body>
