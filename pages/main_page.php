5df7763ad0e925ed0c837af79ca9a3b3e468eb4e
Jonathan Dingess <J_And_M33@yahoo.com>
2017-04-15 16:29:42 -0400

    CSS stuff

---
+++ pages/main_page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Suck my dick </title>
+		<meta charset="UTF-8">
+		
+		<?php
+			//Headers here
+		?>
+		<link rel="stylesheet" href="../css/main_page.css" type="text/css">
+		
+		
+	</head>
+	<body>
+		<div id="main-page" class="container">
+			<div class="main-page-title">
+				<h1>
+					Suck My Donkey Dong
+				</h1>
+			</div>
+		</div>
+	</body>
+</html>
