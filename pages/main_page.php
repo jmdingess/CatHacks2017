416531b5ec0f57be3cfc09be86a1a83ae30eae3d
Jonathan Dingess <J_And_M33@yahoo.com>
2017-04-16 04:17:19 -0400

    Maybe this works

--- pages/main_page.php
+++ pages/main_page.php
@@ -4,11 +4,11 @@
 		<title> Tweet Street </title>
 		<meta charset="UTF-8">
 		
-		<!-- Javascript headers -->
+		<!-- Javascript headers 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<!-- Latest compiled and minified JavaScript -->
+		<!-- Latest compiled and minified JavaScript 
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-		
+		-->
 		<?php
 			//Headers here
 			
@@ -22,8 +22,12 @@
 				
 			
 			/* Accessing Twitter API */
-	
+			
+			if (file_exists(__DIR__ . '/../TwitterOAuth/vendor/autoload.php') ) {
 			require __DIR__ . '/../TwitterOAuth/vendor/autoload.php';
+			} else {
+				die("I was right")
+			}
 
 			use TwitterOAuth\Auth\SingleUserAuth;
 
